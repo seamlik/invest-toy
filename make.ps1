f4bde33239a49365d4a0bf2224f5e171d2bc3094
殷啟聰 | Kai-Chung Yan <seamlikok@gmail.com>
2023-01-05 21:46:40 +0100

    Remove unused PowerShell tasks

--- make.ps1
+++ make.ps1
@@ -5,14 +5,6 @@ switch ($args[0]) {
     format {
         Format-All
     }
-    run {
-        cargo run
-        StopIfLastCommandFailed
-    }
-    check {
-        cargo test
-        StopIfLastCommandFailed
-    }
     cov {
         pwsh task/cov.ps1
     }
