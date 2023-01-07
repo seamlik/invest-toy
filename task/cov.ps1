3ef784eaf38c5a4d7afecf67ef3a2c60c22a4a3f
殷啟聰 | Kai-Chung Yan <seamlikok@gmail.com>
2023-01-07 16:35:02 +0100

    Build code coverage in a different target directory

--- task/cov.ps1
+++ task/cov.ps1
@@ -2,20 +2,22 @@
 
 Write-Output "Running tests and generating code coverage report"
 
-$COV_DIRECTORY = "target/coverage/html"
+$TARGET_DIRECTORY = "target-coverage"
+Remove-Item -Recurse -Path $TARGET_DIRECTORY
+$COV_DIRECTORY = "$TARGET_DIRECTORY/coverage/html"
 New-Item -ItemType Directory $COV_DIRECTORY
 
-$PROFRAW_DIRECTORY = "target/profraw"
-Remove-Item -Recurse -Path $PROFRAW_DIRECTORY
+$PROFRAW_DIRECTORY = "$TARGET_DIRECTORY/profraw"
 New-Item -ItemType Directory -Path $PROFRAW_DIRECTORY
 $PROFRAW_DIRECTORY_ABSOLUTE = Resolve-Path -Path $PROFRAW_DIRECTORY
 
 $Env:RUSTFLAGS = "--codegen instrument-coverage"
 $Env:LLVM_PROFILE_FILE = "$PROFRAW_DIRECTORY_ABSOLUTE/default-%p-%m.profraw"
+$Env:CARGO_TARGET_DIR = $TARGET_DIRECTORY
 cargo test
 StopIfLastCommandFailed
 
-grcov --branch --binary-path ./target/debug/deps/ --output-type html --source-dir . --output-path $COV_DIRECTORY .
+grcov --branch --binary-path $TARGET_DIRECTORY/debug/deps/ --output-type html --source-dir . --output-path $COV_DIRECTORY .
 StopIfLastCommandFailed
 
 Start-Process "$COV_DIRECTORY/index.html"
