7efa5ff08b735c9aebff06499262679f286d8670
seamlik <seamlikok@gmail.com>
2025-03-09 19:03:38 +0900

    Extract stock URLs

--- web-extension/src/yahoo-finance.ts
+++ web-extension/src/yahoo-finance.ts
@@ -1,26 +1,64 @@
 export async function generateReport() {
-  const tab = await chrome.tabs.create({
-    url: "https://finance.yahoo.com/portfolios",
-  });
-  const tabId = tab.id;
-  if (typeof tabId !== "number") {
-    throw new Error("Active tab has no ID");
-  }
-  await visitPortfolio(tabId);
+  const tabIdPortfolio = await visitPortfolio();
+  const urls = await extractStockUrls(tabIdPortfolio);
+  console.info(`Found ${urls.length.toString()} stocks`);
 }
 
-async function visitPortfolio(tabId: number) {
-  const portfolioUrlResult = await chrome.scripting.executeScript({
-    target: { tabId: tabId },
+async function visitPortfolio(): Promise<number> {
+  const tabIdPortfolioList = await navigateTo(
+    "https://finance.yahoo.com/portfolios",
+  );
+  const portfolioUrlResults = await chrome.scripting.executeScript({
+    target: { tabId: tabIdPortfolioList },
     func: queryFirstPortfolioUrl,
   });
-  const portfolioUrl = portfolioUrlResult[0].result as string;
-  await chrome.tabs.update(tabId, { url: portfolioUrl });
+  const portfolioUrl = portfolioUrlResults[0].result as string;
+  console.info(`Navigating to portfolio URL: ${portfolioUrl}`);
+  await chrome.tabs.remove(tabIdPortfolioList);
+  return await navigateTo(portfolioUrl);
 }
 
-function queryFirstPortfolioUrl(): string | null {
+function queryFirstPortfolioUrl(): string {
   const element = document.querySelector(
     'table[data-testid="table-container"] tbody tr td a.primary-link',
   );
-  return element instanceof HTMLAnchorElement ? element.href : null;
+  if (element instanceof HTMLAnchorElement) {
+    return element.href;
+  } else {
+    throw new Error("Could not find the anchor to portfolio");
+  }
+}
+
+async function extractStockUrls(tabId: number): Promise<string[]> {
+  const stockUrlResults = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: queryAllStockUrls,
+  });
+  return stockUrlResults[0].result as string[];
+}
+
+function queryAllStockUrls(): string[] {
+  const elements = document.querySelectorAll(
+    "table > tbody > tr > td:first-child a",
+  );
+  const urls: string[] = [];
+  for (const anchor of elements) {
+    if (anchor instanceof HTMLAnchorElement) {
+      urls.push(anchor.href);
+    } else {
+      throw new Error(`Element is not an anchor: ${anchor.getHTML()}`);
+    }
+  }
+  return urls;
+}
+
+async function navigateTo(url: string): Promise<number> {
+  const tab = await chrome.tabs.create({
+    url: url,
+  });
+  const tabId = tab.id;
+  if (typeof tabId !== "number") {
+    throw new Error("Tab has no ID");
+  }
+  return tabId;
 }
