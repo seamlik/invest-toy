ae68bdba0a0af3244b7e75bba9f8723e906dd3a8
seamlik <seamlikok@gmail.com>
2025-03-10 19:45:06 +0900

    Fix ESLint issues

--- web-extension/src/service-worker.ts
+++ web-extension/src/service-worker.ts
@@ -1,5 +1,8 @@
 import { generateReport } from "./yahoo-finance.js";
 
 chrome.action.onClicked.addListener((tab) => {
-  generateReport();
+  console.info(`Service Worker clicked on tab ${tab.id?.toString() ?? "null"}`);
+  generateReport().catch((e: unknown) => {
+    console.error(e);
+  });
 });
